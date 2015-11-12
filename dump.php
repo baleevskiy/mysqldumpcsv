6d0ca0de00c0c3bdbf5df3dad4a9649f285344b9
undercover <baleevskii@gmail.com>
2015-11-13 01:01:38 +0300

    main Command

--- dump.php
+++ dump.php
@@ -11,15 +11,16 @@ abstract class Command implements Executable{
     public function __construct($params){
         $this->_params = $params;
     }
+
+    public function getParam($key, $default=null){
+        return isset($this->_params[$key])?$this->_params[$key]:$default;
+    }
 }
 
 class DumpCommand extends Command{
-
     public function execute(){
 
     }
-
-
 }
 
 
@@ -31,6 +32,37 @@ class CreateTableCommand extends Command{
 
 
 class PrintHelpCommand extends Command{
+    private $__doc=<<<EOT
+--file [csv file name] – this is the name of the CSV to be parsed
+
+--create_table – this will cause the MySQL users table to be built (and no further
+
+action will be taken)
+
+--dry_run – this will be used with the --file directive in the instance that we want
+
+to run the script but not insert into the DB. All other functions will be executed,
+
+but the database won't be altered.
+
+-u – MySQL username
+
+-p – MySQL password
+
+-h – MySQL host
+
+--help – which will output the above list of directives with details.
+
+EOT;
+
+    public function execute(){
+        print $this->__doc;
+        return True;
+    }
+}
+
+
+class mainCommand extends Command {
     public function execute(){
 
     }
--- mysqldumpcsv.php
+++ mysqldumpcsv.php
@@ -10,28 +10,7 @@ $opts = getopt ('u:p:h:' , [
 ] );
 
 
-$docs=<<<EOT
---file [csv file name] – this is the name of the CSV to be parsed
 
---create_table – this will cause the MySQL users table to be built (and no further
-
-action will be taken)
-
---dry_run – this will be used with the --file directive in the instance that we want
-
-to run the script but not insert into the DB. All other functions will be executed,
-
-but the database won't be altered.
-
--u – MySQL username
-
--p – MySQL password
-
--h – MySQL host
-
---help – which will output the above list of directives with details.
-
-EOT;
 
 if(isset($opts['help'])){
     die($docs);
